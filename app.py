ac9f76e7853f9963d4654da43bfdb608b0c6d281
lty2002 <liangtianyi2002@foxmail.com>
2023-09-07 00:06:24 +0800

    fix: fix known bugs

--- app.py
+++ app.py
@@ -9,18 +9,14 @@ app = Flask(__name__)
 
 @app.route('/ics/<json_type>', methods=['POST'])
 def ics(json_type):
-    content_type = request.headers.get('Content-Type')
-    if content_type != 'application/json':
-        return 'Content-Type not supported!'
-    else:
-        body = request.get_json()
+    body = request.get_json()
 
-        semester = body.get('semester')
-        timetable = body.get('data')
-        if SEMESTERS_START_DAY.get(semester) is not None:
-            return chengzi2ics(semester, timetable) \
-                if json_type == 'chengzi' else mini2ics(semester, timetable)
-        return 'Invalid semester!'
+    semester = body.get('semester')
+    timetable = body.get('data')
+    if SEMESTERS_START_DAY.get(semester) is not None:
+        return chengzi2ics(semester, timetable) \
+            if json_type == 'chengzi' else mini2ics(semester, timetable)
+    return 'Invalid semester!'
 
 
 if __name__ == '__main__':
